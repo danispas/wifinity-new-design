dd489252309080fbf0db61984b66321c6634e559
danispas <danijel.spasic@wifinity.co.uk>
2026-08-03 10:47:54 +0200

    add speed test mobile

--- app-d1/main.js
+++ app-d1/main.js
@@ -13,3 +13,50 @@ document
     });
 
 });
+
+const modal = document.getElementById('speedTestModal');
+const openButton = document.getElementById('openSpeedTestModal');
+const closeButton = document.getElementById('closeSpeedTestModal');
+const overlay = modal.querySelector('.w-full-screen-modal__overlay');
+
+openButton.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  modal.classList.add(
+    'w-full-screen-modal--open'
+  );
+
+  document.body.style.overflow = 'hidden';
+})
+
+function closeModal() {
+  modal.classList.remove(
+    'w-full-screen-modal--open'
+  );
+
+  document.body.style.overflow = '';
+}
+
+closeButton.addEventListener(
+  'click',
+  closeModal
+);
+
+overlay.addEventListener(
+  'click',
+  closeModal
+);
+
+document.addEventListener(
+  'keydown',
+  function (event) {
+    if (
+      event.key === 'Escape' &&
+      modal.classList.contains(
+        'w-full-screen-modal--open'
+      )
+    ) {
+      closeModal();
+    }
+  }
+);
